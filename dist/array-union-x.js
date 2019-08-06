62571486dd04a5fd7972a5360422b1c682fab36b
Graham Fairweather <xotic750@gmail.com>
2019-08-06 23:27:17 +0200

    Add coveralls badge

--- dist/array-union-x.js
+++ dist/array-union-x.js
@@ -2,7 +2,7 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2017",
-  "date": "2019-08-06T01:00:01.097Z",
+  "date": "2019-08-06T21:27:09.817Z",
   "describe": "",
   "description": "Creates an array of unique values, in order, from all given arrays.",
   "file": "array-union-x.js",
